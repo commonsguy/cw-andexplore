f0bbe6175171c60d96edd23a537040926d694a58
Mark Murphy <mmurphy@commonsware.com>
2018-01-28 19:15:00 -0500

    new tutorial

---
+++ T12-Repository/ToDo/app/src/main/java/com/commonsware/todo/ToDoModel.java
@@ -0,0 +1,36 @@
+package com.commonsware.todo;
+
+import com.google.auto.value.AutoValue;
+import java.util.Calendar;
+import java.util.UUID;
+import android.support.annotation.Nullable;
+
+@AutoValue
+public abstract class ToDoModel {
+  public abstract String id();
+  public abstract boolean isCompleted();
+  public abstract String description();
+  @Nullable public abstract String notes();
+  public abstract Calendar createdOn();
+
+  static Builder builder() {
+    return(new AutoValue_ToDoModel.Builder());
+  }
+
+  public static Builder creator() {
+    return(builder()
+      .isCompleted(false)
+      .id(UUID.randomUUID().toString())
+      .createdOn(Calendar.getInstance()));
+  }
+
+  @AutoValue.Builder
+  public static abstract class Builder {
+    abstract Builder id(String id);
+    public abstract Builder isCompleted(boolean isCompleted);
+    public abstract Builder description(String desc);
+    public abstract Builder notes(String notes);
+    abstract Builder createdOn(Calendar date);
+    public abstract ToDoModel build();
+  }
+}
