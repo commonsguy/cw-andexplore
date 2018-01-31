7a91c993717045704d6b9a149cb1eccf9378d4ea
Mark Murphy <mmurphy@commonsware.com>
2018-01-31 14:37:47 -0500

    new tutorial

---
+++ T18-Edit/ToDo/app/src/main/java/com/commonsware/todo/EditFragment.java
@@ -0,0 +1,48 @@
+package com.commonsware.todo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.commonsware.todo.databinding.TodoEditBinding;
+
+public class EditFragment extends Fragment {
+  private static final String ARG_ID="id";
+  private TodoEditBinding binding;
+
+  static EditFragment newInstance(ToDoModel model) {
+    EditFragment result=new EditFragment();
+
+    if (model!=null) {
+      Bundle args=new Bundle();
+
+      args.putString(ARG_ID, model.id());
+      result.setArguments(args);
+    }
+
+    return(result);
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater,
+                           @Nullable ViewGroup container,
+                           @Nullable Bundle savedInstanceState) {
+    binding=TodoEditBinding.inflate(getLayoutInflater(), container, false);
+
+    return(binding.getRoot());
+  }
+
+  @Override
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    binding.setModel(ToDoRepository.get().find(getModelId()));
+  }
+
+  private String getModelId() {
+    return(getArguments().getString(ARG_ID));
+  }
+}
---
+++ T18-Edit/ToDo/app/src/main/java/com/commonsware/todo/MainActivity.java
@@ -0,0 +1,54 @@
+package com.commonsware.todo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends FragmentActivity
+  implements RosterListFragment.Contract, DisplayFragment.Contract {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    if (getSupportFragmentManager().findFragmentById(android.R.id.content)==null) {
+      getSupportFragmentManager().beginTransaction()
+        .add(android.R.id.content, new RosterListFragment())
+        .commit();
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.actions, menu);
+
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if (item.getItemId()==R.id.about) {
+      startActivity(new Intent(this, AboutActivity.class));
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  public void showModel(ToDoModel model) {
+    getSupportFragmentManager().beginTransaction()
+      .replace(android.R.id.content, DisplayFragment.newInstance(model))
+      .addToBackStack(null)
+      .commit();
+  }
+
+  @Override
+  public void editModel(ToDoModel model) {
+    getSupportFragmentManager().beginTransaction()
+      .replace(android.R.id.content, EditFragment.newInstance(model))
+      .addToBackStack(null)
+      .commit();
+  }
+}
